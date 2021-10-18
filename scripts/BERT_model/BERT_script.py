be07e63b1fc821cc645f4885833977d66b1fa5e9
ragouradja <radja.ragou@gmail.com>
2021-10-18 07:40:49 +0200

    BERT model with PCA

--- scripts/BERT_model/BERT_script.py
+++ scripts/BERT_model/BERT_script.py
@@ -1,17 +1,39 @@
-import tokenization # tokenization.py
-import tensorflow as tf
-import tensorflow_hub as hub
-from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
+
 import numpy as np
 import pandas as pd
-import nltk
-import transformers
-import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 import re
-nltk.download("stopwords")
-from nltk.corpus import stopwords
 
+import warnings
+
+warnings.filterwarnings('ignore')
+
+from tensorflow.keras.optimizers import Adam
+import tensorflow as tf
+from tensorflow import keras
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.models import load_model
+import nltk
+# nltk.download('punkt')
+# nltk.download('wordnet')
+# nltk.download("stopwords")
+from nltk.corpus import stopwords
+import time
+import torch
+from keras.preprocessing.text import Tokenizer
+
+import os
+import sys
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem import WordNetLemmatizer
+from joblib import Parallel, delayed
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import transformers as ppb
+import matplotlib.pyplot as plt
 
 
 def pre_processing(data):
@@ -28,54 +50,6 @@ def pre_processing(data):
 
     return data
 
-def bert_encode(texts, tokenizer, max_len=512):
-    all_tokens = []
-    all_masks = []
-    all_segments = []
-    
-    for text in texts:
-        text = tokenizer.tokenize(text)
-        
-        text = text[:max_len-2]
-        input_sequence = ["[CLS]"] + text + ["[SEP]"]
-        pad_len = max_len-len(input_sequence)
-        
-        tokens = tokenizer.convert_tokens_to_ids(input_sequence) + [0] * pad_len
-        pad_masks = [1] * len(input_sequence) + [0] * pad_len
-        segment_ids = [0] * max_len
-        
-        all_tokens.append(tokens)
-        all_masks.append(pad_masks)
-        all_segments.append(segment_ids)
-        
-    return np.array(all_tokens), np.array(all_masks), np.array(all_segments)
-
-def build_model(bert_layer, max_len=512):
-    input_word_ids = tf.keras.Input(shape=(max_len,), dtype=tf.int32, name="input_word_ids")
-    input_mask = tf.keras.Input(shape=(max_len,), dtype=tf.int32, name="input_mask")
-    segment_ids = tf.keras.Input(shape=(max_len,), dtype=tf.int32, name="segment_ids")
-    
-    pooled_output, sequence_output = bert_layer([input_word_ids, input_mask, segment_ids])
-    
-    #clf_output = sequence_output[:, 0, :]
-    
-    # CNN model
-    
-    net = tf.keras.layers.Conv1D(124, (5), activation='relu')(sequence_output)
-    net = tf.keras.layers.MaxPooling1D(2)(net)
-    
-#     net = tf.keras.layers.Conv1D(64, (5), activation='relu')(net)
-#     net = tf.keras.layers.GlobalMaxPooling1D()(net)
-    
-
-    net = tf.keras.layers.Dense(20, activation="relu")(net)
-    net = tf.keras.layers.Flatten()(net)    
-    net = tf.keras.layers.Dropout(0.1)(net)
-    out = tf.keras.layers.Dense(9, activation="softmax")(net)
-    
-    model = tf.keras.models.Model(inputs=[input_word_ids, input_mask, segment_ids], outputs=out)
-    model.compile(tf.keras.optimizers.Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
-    return model
 
 
 def get_data(data_file):
@@ -86,40 +60,111 @@ def get_data(data_file):
     return X, dataset
 
 
-def model(data_file):
-    X, dataset = get_data(data_file)
-    tokenizer = transformers.BertTokenizer.from_pretrained('bert-base-uncased')
+def lemm(text):
+    lemmatizer = WordNetLemmatizer()
+    temp = []
+    list_words = list(set(word_tokenize(text)))
+    for word in list_words:
+        temp.append(lemmatizer.lemmatize(word))
+    return list(set(temp))
+            
 
-    m_url = 'https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/2'
-    bert_layer = hub.KerasLayer(m_url, trainable=True)
-
-    vocab_file = bert_layer.resolved_object.vocab_file.asset_path.numpy()
-    do_lower_case = bert_layer.resolved_object.do_lower_case.numpy()
-    tokenizer = tokenization.FullTokenizer(vocab_file, do_lower_case)
-
-    Y_D = pd.get_dummies(dataset['Class']).values
-    XD_train, XD_test, YD_train, YD_test = train_test_split(X, Y_D, test_size = 0.2, random_state = 42, stratify=Y_D)
-    print(XD_train.shape, YD_train.shape)
-    print(XD_test.shape, YD_test.shape)
+def get_features(article, tokenizer, model):
+    token = []
+    max_len = 0
     
-    train_input = bert_encode(XD_train, tokenizer, max_len=500)
-    test_input = bert_encode(XD_test, tokenizer, max_len=500)  
+    for chunk in lemm(article):
+        token.append(tokenizer.encode(chunk, add_special_tokens=True))
+        
+    for i in token:
+        if len(i) > max_len:
+            max_len = len(i)
 
-    model = build_model(bert_layer, max_len=500)
+    padded = np.array([i + [0]*(max_len-len(i)) for i in token])
+        
+    attention_mask = np.where(padded != 0, 1, 0)
+    attention_mask.shape
+    
+    input_ids = torch.tensor(padded)  
+    attention_mask = torch.tensor(attention_mask)
+
+
+    with torch.no_grad():
+        last_hidden_states = model(input_ids, attention_mask=attention_mask)
+        
+    features = last_hidden_states[0][:,0,:].numpy()
+    
+    print("SHAPE" ,features.shape)
+    tsne = PCA(n_components=50)
+    tsne_results = tsne.fit_transform(features.T)
+    
+    return tsne_results.T.flatten()
+
+def do_features_parallel(all_articles, args):
+
+    model_class, tokenizer_class, pretrained_weights = (ppb.DistilBertModel, ppb.DistilBertTokenizer, 'distilbert-base-uncased')
+    #model_class, tokenizer_class, pretrained_weights = (ppb.BertModel, ppb.BertTokenizer, 'bert-base-uncased')
+    # Load pretrained model/tokenizer
+    tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
+    model = model_class.from_pretrained(pretrained_weights)
+    start = time.time()
+    all_vec = Parallel(n_jobs= args.cpu, verbose = 0, prefer="processes")(delayed(get_features)
+    (article, tokenizer, model) for article in all_articles)
+    print("TIME : ",time.time() - start)
+    final_dtf = pd.DataFrame(list(all_vec))
+    return final_dtf
+
+# def testing_parallel(args):
+#     all_articles, dataset = get_data(args.data_file)
+
+#     dtf = do_features_parallel(all_articles.head(100), args)
+#     print(dtf)
+
+def model(args):
+    clean_text, clean_dtf = get_data(args.data_file)
+
+    labels = pd.get_dummies(clean_dtf['Class'].head(10)).values
+    features = do_features_parallel(clean_text.head(10), args)
+    features.to_csv("../../datas/tsne_data.csv")
+    XD_train, XD_test, YD_train, YD_test = train_test_split(features,
+     labels, test_size = 0.2, random_state = 42)
+
+    print(XD_train.shape, YD_train.shape)
+    XD_train = XD_train.values.reshape(XD_train.shape[0],XD_train.shape[1],1)
+    # YD_train = YD_train.reshape(YD_train.shape[0],7)
+
+    model = Sequential([
+    layers.Conv1D(32, kernel_size = [3], activation = "relu", input_shape = (XD_train.shape[1],1)),
+    layers.MaxPooling1D(pool_size = [2]),
+    layers.Conv1D(64, kernel_size = [3], activation = "relu"),
+    layers.MaxPooling1D(pool_size = [2]),
+    layers.Flatten(),
+    layers.Dropout(0.5),
+    layers.Dense(units = YD_train.shape[1],  activation = "softmax")      
+    ])
     model.summary()
 
-    checkpoint = tf.keras.callbacks.ModelCheckpoint('../../results/model_bert.h5', monitor='val_accuracy', save_best_only=True, verbose=1)
-    earlystopping = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=3,verbose=1)
+
+
+    model.compile(
+    loss = 'categorical_crossentropy',
+    optimizer = "adam",
+    metrics = ["accuracy"]
+    )
+    checkpoint = tf.keras.callbacks.ModelCheckpoint('../../results/model_BERT.h5', monitor='val_accuracy', save_best_only=True, verbose=1)
+    earlystopping = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=5, verbose=1)
 
     train_sh = model.fit(
-        train_input, YD_train,
+        XD_train, YD_train,
         validation_split=0.2,
-        epochs=20,
-        callbacks=[checkpoint, earlystopping],
+        epochs=35,
+        callbacks=[checkpoint,earlystopping],
         batch_size=32,
         verbose=1
-        )
+    )
 
+
+    
     plt.figure(1)
     plt.plot(train_sh.history['loss'])
     plt.plot(train_sh.history['val_loss'])
@@ -140,6 +185,9 @@ def model(data_file):
 
 
 
+
 if __name__ == "__main__":
-    data_file = "../../../data/data_stat.txt"
+    configuration = tf.compat.v1.ConfigProto(device_count={"GPU": 0})
+    session = tf.compat.v1.Session(config=configuration)    
+    data_file = "../../datas/all_data_clean.txt"
     model(data_file)
--- scripts/BERT_model/launch_bert.py
+++ scripts/BERT_model/launch_bert.py
@@ -1,5 +1,9 @@
 import BERT_script
 import argparse
+import os
+import tensorflow as tf
+import sys
+import multiprocessing as mp
 
 def main():
     parser = argparse.ArgumentParser()
@@ -8,10 +12,51 @@ def main():
                         type=str, 
                         default="../../datas/all_data_clean.txt", 
                         help='Cleaned articles')
+    parser.add_argument('-c',
+                        dest = "cpu",                  
+                        type=int, 
+                        default=-1, 
+                        help='Cleaned articles')                        
 
     args = parser.parse_args()
-    print(args)
-    BERT_script.model(args.data_file)
+    
+    cpu = args.cpu
+
+    if cpu != -1 and cpu <= 0:
+        sys.exit(f"Can't use {cpu} CPU. Please enter a valid number (>=1 or -1) of CPU to use.")
+    else:
+        max_cpu = mp.cpu_count()
+        if cpu == -1:
+            n_cpu = mp.cpu_count() - 1
+            args.cpu = n_cpu
+        elif cpu >= 1:			
+            if cpu >= max_cpu:
+                args.cpu = max_cpu
+
+    print(f"\nUsing {args.cpu} CPUs for parallel jobs.")
+
+
+    physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    print(physical_devices)
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+    config = tf.compat.v1.ConfigProto()
+    config.gpu_options.allow_growth = True 
+    config.gpu_options.per_process_gpu_memory_fraction = 0.7
+
+    session = tf.compat.v1.Session(config=config)
+    os.environ["TF_GPU_ALLOCATOR"] = "cuda_malloc_async"
+    print(tf.__version__)
+    with tf.device('/gpu:0'):
+        BERT_script.model(args)
+
+
+    # 379s 100row 1 cpu
+    # 293s 100 row 5 cpu
+    # 277s
+
+
+
 
 if __name__ == "__main__":
     main()
