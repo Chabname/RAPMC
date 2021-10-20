48e1f628725c829ed69824f5de31985451abf41c
ragouradja <radja.ragou@gmail.com>
2021-10-21 00:38:28 +0200

    scibert

--- scripts/BERT_model/BERT_script.py
+++ scripts/BERT_model/BERT_script.py
@@ -1,10 +1,15 @@
 
 import numpy as np
+from numpy.core.fromnumeric import size
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import re
 
 import warnings
+
+from tensorflow.python.keras.layers.core import Dropout
+from tensorflow.python.keras.layers.normalization.batch_normalization import BatchNormalization
+from transformers.utils.dummy_pt_objects import NoBadWordsLogitsProcessor
 
 warnings.filterwarnings('ignore')
 
@@ -34,12 +39,13 @@ from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 import transformers as ppb
 import matplotlib.pyplot as plt
+import math
 
 
 def pre_processing(data):
     sw = stopwords.words("english")
     # lowercase text
-    data = data.apply(lambda x: " ".join(i.lower() for i in  str(x).split()))
+    # data = data.apply(lambda x: " ".join(i.lower() for i in  str(x).split()))
 #     # remove numeric values
 #     data = data.str.replace("\d","")
 #     # remove punctuations
@@ -47,7 +53,7 @@ def pre_processing(data):
     # remove stopwords: the,a,an etc.
     data = data.apply(lambda x: " ".join(i for i in x.split() if i not in sw))
     data = data.apply(lambda x: re.sub("⇓","",x))
-
+    data = data.apply(lambda x: re.sub(",|\)|\(|\.","",x))
     return data
 
 
@@ -68,89 +74,164 @@ def lemm(text):
         if len(word) > 2:
             temp.append(lemmatizer.lemmatize(word))
     return list(set(temp))
+
+
+
+def get_chunks(text_split):
+    length_text = len(text_split)
+    size = int(len(text_split) / 20)
+    final = []
+    for i in range(0, length_text, size):
+        final += [" ".join(text_split[i:i+size])]
+    return final
+
+
+
+def get_features(article, tokenizer, model):
+    nb_chunk = 20
+    chunked_article = get_chunks(article)
+
+    features = []
+    for c in range(nb_chunk):
+        token = []
+        max_len = 0
+        token.append(tokenizer.encode(chunked_article[c], add_special_tokens=True))
+        for i in token:
+            if len(i) > max_len:
+                max_len = len(i)
+
+
+        padded = np.array([i + [0]*(max_len-len(i)) for i in token])
+        input_ids = torch.tensor(padded)  
+
+        if len(padded[0]) > 500:
+            padded = np.array([padded[0][:500]])
+            if padded[0][-1] != 103:
+                padded = np.append(padded[0],(103))
+        
             
+        attention_mask = np.where(padded != 0, 1, 0)                       
+        attention_mask = torch.tensor(attention_mask)
+        try:
+            attention_mask.shape[1]
+        except:
+            attention_mask = attention_mask.reshape(1, attention_mask.shape[0])
 
-def get_features(lemmatized_text, tokenizer, model):
-    token = []
-    max_len = 0
-    for chunk in lemmatized_text:
+        with torch.no_grad():
+            last_hidden_states = model(input_ids, attention_mask=attention_mask)
 
-        token.append(tokenizer.encode(chunk, add_special_tokens=True))
+        features.append(last_hidden_states[0][:,0,:].numpy()[0])
+    array = np.array(features)
+    print("SHAPE", array.shape)
+    return array.flatten()
 
-    for i in token:
-        if len(i) > max_len:
-            max_len = len(i)
+def get_outlier(line, tokenizer):
+    nb_chunk = 20
+    if len(line) < 20:
+        return 0
+    return 1
+    #     chunked_article = get_chunks(line["Text"].split())
 
-    padded = np.array([i + [0]*(max_len-len(i)) for i in token])
-        
-    attention_mask = np.where(padded != 0, 1, 0)
-    attention_mask.shape
-    
-    input_ids = torch.tensor(padded)  
-    attention_mask = torch.tensor(attention_mask)
+    #     for c in range(nb_chunk):
+    #         token = []
+    #         max_len = 0
+    #         token.append(tokenizer.encode(chunked_article[c], add_special_tokens=True))
+    #         for i in token:
+    #             if len(i) > max_len:
+    #                 max_len = len(i)
 
-
-    with torch.no_grad():
-        last_hidden_states = model(input_ids, attention_mask=attention_mask)
-        
-    features = last_hidden_states[0][:,0,:].numpy()
-    
-    tsne = PCA(n_components=50)
-    tsne_results = tsne.fit_transform(features.T)
-    print("SHAPE" ,features.shape, tsne_results.shape)
-    
-    return tsne_results.T.flatten()
+    #         padded = np.array([i + [0]*(max_len-len(i)) for i in token])
+    #         if len(padded[0]) > 500:
+    #             padded = np.array([padded[0][:500]])
+    #             if padded[0][-1] != 103:
+    #                 padded = np.append(padded[0],(103))
+            
+                
+    #         attention_mask = np.where(padded != 0, 1, 0)                       
+    #         attention_mask = torch.tensor(attention_mask)
+    #         try:
+    #             attention_mask.shape[1]
+    #         except:
+    #             attention_mask = attention_mask.reshape(1, attention_mask.shape[0])
+            
+    #     if attention_mask.shape[1] > 512:
+    #         return 2
+    #     return 1
+    # return 0
 
 def do_features_parallel(all_articles, args):
 
-    model_class, tokenizer_class, pretrained_weights = (ppb.DistilBertModel, ppb.DistilBertTokenizer, 'distilbert-base-uncased')
-    #model_class, tokenizer_class, pretrained_weights = (ppb.BertModel, ppb.BertTokenizer, 'bert-base-uncased')
-    # Load pretrained model/tokenizer
-    tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
-    model = model_class.from_pretrained(pretrained_weights)
+    # #model_class, tokenizer_class, pretrained_weights = (ppb.DistilBertModel, ppb.DistilBertTokenizer, 'distilbert-base-uncased')
+    # model_class, tokenizer_class, pretrained_weights = (ppb.BertModel, ppb.BertTokenizer, 'bert-base-uncased')
+    # # Load pretrained model/tokenizer
+    # tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
+    # model = model_class.from_pretrained(pretrained_weights)
+    tokenizer_scibert = ppb.AutoTokenizer.from_pretrained('allenai/scibert_scivocab_uncased')
+    model_scibert = ppb.AutoModel.from_pretrained('allenai/scibert_scivocab_uncased')
+        
+    out = all_articles.apply(lambda ar: get_outlier(ar, tokenizer_scibert))
+    to_keep = out[out == 1].index
+    
     start = time.time()
     all_vec = Parallel(n_jobs= args.cpu, verbose = 0, prefer="processes")(delayed(get_features)
-    (article, tokenizer, model) for article in all_articles)
+    (article, tokenizer_scibert, model_scibert) for article in all_articles.loc[to_keep])
     print("TIME : ",time.time() - start)
     final_dtf = pd.DataFrame(list(all_vec))
     return final_dtf
 
-def model(args):
+
+def get_input_data(args):
     clean_text, clean_dtf = get_data(args.data_file)
-    lemmatized_text = clean_text.apply(lambda line: lemm(line))
-    nb_word = lemmatized_text.apply(lambda line: len(line))
+    clean_text = pre_processing(clean_text)
+    split_text = clean_text.apply(lambda line: line.split())
+    clean_dtf["Text"] = split_text
 
-    sup_50 = nb_word[nb_word > 50].index
+    # clean_text_010, clean_dtf_010 = get_data(args.data_file_010)
+    # clean_text_010 = clean_text_010.apply(lambda line: pre_processing(line))
+    # split_text_010 = clean_text_010.apply(lambda line: line.split())
+    # out_010 = clean_dtf_010.loc[out[out == 2].index].apply(lambda line: get_outlier(line), axis = 1)
+    
 
-    labels = pd.get_dummies(clean_dtf.loc[sup_50,'Class']).values
-    features = do_features_parallel(lemmatized_text.loc[sup_50,], args)
-    features.to_csv("../../datas/pca_50_data.csv")
+    features = do_features_parallel(clean_dtf["Text"], args)
+    labels = pd.get_dummies(clean_dtf.loc[features.index,'Class']).values
+
+    features.to_pickle("../../../data/array_data_scibert.pkl")
+    np.save("../../../data/labels_scibert.npy",labels)
+    return features, labels
+
+def model(args):
+    features, labels = get_input_data(args)
+
+    # features = pd.read_pickle("../../../data/array_data_scibert.pkl")
+    # labels = np.load("../../../data/labels_scibert.npy")
     XD_train, XD_test, YD_train, YD_test = train_test_split(features,
-     labels, test_size = 0.2, random_state = 42)
+     labels, test_size = 0.2, random_state = 42, stratify=labels)
 
     print(XD_train.shape, YD_train.shape)
     XD_train = XD_train.values.reshape(XD_train.shape[0],XD_train.shape[1],1)
     # YD_train = YD_train.reshape(YD_train.shape[0],7)
 
     model = Sequential([
-    layers.Conv1D(32, kernel_size = [3], activation = "relu", input_shape = (XD_train.shape[1],1)),
-    layers.MaxPooling1D(pool_size = [2]),
-    layers.Conv1D(64, kernel_size = [3], activation = "relu"),
-    layers.MaxPooling1D(pool_size = [2]),
-    layers.Flatten(),
-    layers.Dropout(0.5),
-    layers.Dense(units = YD_train.shape[1],  activation = "softmax")      
+        layers.Conv1D(128, 2, activation='relu',strides = 2, input_shape = (XD_train.shape[1],1)),
+        layers.MaxPooling1D(),
+        layers.Conv1D(256, 2, activation='relu',strides = 2),
+        layers.MaxPooling1D(2, strides=2),
+        # layers.LSTM(64, recurrent_dropout=0.2, dropout=0.2,input_shape = (XD_train.shape[1],1)),
+        layers.Flatten(),
+        layers.Dense(36, activation='relu'),
+        layers.Dropout(0.2),
+        layers.Dense(9, activation='softmax')
     ])
     model.summary()
 
-
+    # NVIDIA Geforce GTX 1050 TI --> 39 millions de param = ok à 80% du GPU
 
     model.compile(
     loss = 'categorical_crossentropy',
-    optimizer = "adam",
+    optimizer = keras.optimizers.Adam(),
     metrics = ["accuracy"]
     )
-    checkpoint = tf.keras.callbacks.ModelCheckpoint('../../results/model_BERT.h5', monitor='val_accuracy', save_best_only=True, verbose=1)
+    checkpoint = tf.keras.callbacks.ModelCheckpoint(('../../results/model_SCIBERT.h5'), monitor='val_accuracy', save_best_only=True, verbose=1)
     earlystopping = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=5, verbose=1)
 
     train_sh = model.fit(
@@ -158,7 +239,7 @@ def model(args):
         validation_split=0.2,
         epochs=35,
         callbacks=[checkpoint,earlystopping],
-        batch_size=32,
+        batch_size=64,
         verbose=1
     )
 
@@ -171,7 +252,7 @@ def model(args):
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train', 'val'], loc = 'upper left')
-    plt.savefig("../../results/loss_plot.PNG")
+    plt.savefig(("../../results/loss_plot_scibert.PNG"))
 
     plt.figure(1).clear()
     plt.plot(train_sh.history['accuracy'])
@@ -180,13 +261,12 @@ def model(args):
     plt.ylabel('Accuracy')
     plt.xlabel('Epoch')
     plt.legend(['Train', 'Test'], loc='upper left')
-    plt.savefig("../../results/acc_plot.jpg")
-
+    plt.savefig(("../../results/acc_plot_scibert.jpg"))
 
 
 
 if __name__ == "__main__":
     configuration = tf.compat.v1.ConfigProto(device_count={"GPU": 0})
     session = tf.compat.v1.Session(config=configuration)    
-    data_file = "../../datas/all_data_clean.txt"
+    data_file = "../../datas/all_data_clean_011.txt"
     model(data_file)
