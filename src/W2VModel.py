003af72a89f8ff26c26bddb8bc4e6cc1a5a0b194
Chabname <chabname@wanadoo.fr>
2021-10-20 09:19:02 +0200

    continuing models

--- src/W2VModel.py
+++ src/W2VModel.py
@@ -2,9 +2,14 @@ from Model import Model
 from Datas import Articles
 from EmbW2V import EmbW2V
 
+from gensim.models import Word2Vec
+import time
+import multiprocessing
+
 class W2V(Model):
-         
-    def __init__(self, data_file):
+    model_path="results/"
+
+    def __init__(self, data_file, learned_mod_path, win_size, epoch, batch, stopword, repeat, concat):
         """ Initialize Low matrix before completing
 
         Keyword arguments:
@@ -12,9 +17,62 @@ class W2V(Model):
         """
 
         super().__init__(data_file)
+        self.learned_mod_path = learned_mod_path
+        self.win_size = win_size
+        self.epoch = epoch
+        self.batch = batch
+        self.concat = concat
+        self.repeat = repeat
+        self.stopword = stopword
 
-    def pre_processing(self,  win_size, epoch, batch, concat, repeat, clean_stopword):
+
+    def pre_processing(self):
         articles = Articles(self.data_file)
-        learned_model = EmbW2V(articles.datas, win_size, epoch, batch, concat, repeat)
-        learned_model.preprocess_datas(clean_stopword)
-        return learned_model
+        emb_model = EmbW2V(articles.datas, self.win_size, self.epoch, self.batch, self.concat, self.repeat)
+        emb_model.preprocess_datas(self.stopword)
+        return emb_model
+
+
+    def copy_embeded_model(self):
+        model = Word2Vec.load(self.learned_mod_path)
+        split_filename = self.learned_mod_path.split("/")
+        filename = split_filename[len(split_filename) - 1]
+        self.model_path += "Copy_" + filename
+        model.save(self.model_path)
+    
+
+    def train_embeded_model(self):
+        emb_model = self.pre_processing()
+        emb_model.model_path = self.model_path
+        emb_model.train_model()
+        print(emb_model.model_path)
+
+
+def main(data_file, learned_mod_path, type, win_size, epoch, batch, stopword, repeat, concat):
+    print("_________________________Model using Word2Vec_______________________")
+    print("____________________________________________________________________")
+    start_time = time.perf_counter()
+
+    cores = multiprocessing.cpu_count() 
+
+    w2vmodel = W2V(data_file, learned_mod_path, win_size, epoch, batch, concat, repeat, stopword)
+    w2vmodel.copy_embeded_model()
+    w2vmodel.train_embeded_model()
+
+    stop_time = time.perf_counter()
+    print("____________________________________________________________________")
+    print("Word2vec finished in {} seconds".format(stop_time-start_time))
+
+
+main("datas/701_mix_data_clean.txt", 
+        "results/cbow_A701_WS20_E15_B10000_R20_CTrue.model",
+        type = "cbow", 
+        win_size = 20, 
+        epoch = 15, 
+        batch = 10000, 
+        stopword = True, 
+        repeat = 200, 
+        concat = True)
+
+
+    
